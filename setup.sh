1582a04313d7de6ef4e7296b5fe6f1791a441c5e
João Pinheiro <joaom.pinheiro@outlook.com>
2020-07-23 12:44:34 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ubuntu (GNOME) 20.04 LTS setup script.
+
+# Initial Software
+
+sudo apt update
+
+
+# Gotta reboot now:
+sudo apt update && sudo apt upgrade -y
+
+echo $'\n'$"*** All done! Please reboot now. ***"

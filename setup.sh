f998384594faa68f9f1af1adef9ce07c57c56499
João Pinheiro <joaom.pinheiro@outlook.com>
2020-07-31 14:21:57 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,24 @@
 # Ubuntu (GNOME) 20.04 LTS setup script.
 
 # Initial Software
+# zsh, terminator, tmux, tilix
+sudo apt install zsh
+chsh -s $(which zsh)
+sudo apt install tmux
+sudo apt install terminator
+sudo apt install tilix
+
+# gnome tweaks
+sudo apt install gnome-tweaks
+
+# VPN - no command-line
+sudo apt-get install openvpn
+sudo apt-get install network-manager-openvpn-gnome
+sudo apt install resolvconf
+
+# command-line
+sudo apt install -y openvpn dialog python3-pip python3-setuptools
+sudo pip3 install protonvpn-cli
 
 sudo apt update
 
